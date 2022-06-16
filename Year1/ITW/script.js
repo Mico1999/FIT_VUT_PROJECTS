99460ffd1a26c22b26979ebd903e464f52144e40
Marek <marek.micek@azet.sk>
2022-06-16 16:57:52 +0200

    ITW added

---
+++ Year1/ITW/script.js
@@ -0,0 +1,21 @@
+
+jQuery(document).ready(function() {
+
+    /* activation of buttons to load new url*/
+    
+    jQuery("#army-button").click(function() {
+        
+        window.open('https://www.youtube.com/watch?v=pV4yAluHLE8', '_blank');
+    });
+
+    jQuery("#ping-pong-button").click(function() {
+        
+        window.open('https://www.youtube.com/watch?v=wvJ4wh1kwR8', '_blank');
+    });
+
+    jQuery("#company-button").click(function() {
+        
+        window.open('https://www.youtube.com/watch?v=TjcK-LGgtLA', '_blank');
+    });
+
+});
